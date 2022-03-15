35477240fe19ae4f554555c370439d61e55b24e5
Atoomp <86169325+Atoomp@users.noreply.github.com>
2022-03-15 00:43:57 -0400

    Add files via upload

---
+++ alert.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Title(runoob.com)</title>
+</head>
+<body>
+    
+<h1>first page</h1>
+<p>first paragraph</p>
+<script>
+window.alert(5 + 6);
+</script>
+    
+</body>
+</html>
