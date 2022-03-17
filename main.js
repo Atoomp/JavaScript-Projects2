017e2bff0efa4fab8e9c24c6fec1486a287a8bf0
Atoomp <86169325+Atoomp@users.noreply.github.com>
2022-03-17 02:29:08 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Hello World</title>
+        <meta charset="utd-8">
+        <style type="text/css">
+            h1{
+                color:white;
+                background-color: light blue;
+            }
+        </style>
+    </head>
+    <body>
+        <script>
+            alert("No Data");
+        </script>
+
+        <h1>This a Hello World page</h1>
+        <input type="text" name=""/>
+        <input type="number" name=""/>
+        <script type="text/javascript">
+            document.write("Testing the document")
+        </script>
+    </body>
+</html>
