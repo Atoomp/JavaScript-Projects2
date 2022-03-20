65e2ef7c2cc1df2c3f03d3e68521d079c7b27338
Atoomp <86169325+Atoomp@users.noreply.github.com>
2022-03-20 02:51:22 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,8 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Hello World</title>
-        <meta charset="utd-8">
-        <style type="text/css">
-            h1{
-                color:white;
-                background-color: light blue;
-            }
-        </style>
-    </head>
-    <body>
-        <script>
-            alert("No Data");
-        </script>
+// This is my first JavaScript code!
+<script>
+    var text = "Hello World!";
+    document.write("<p>Fontcolor:" + text.fontcolor("Red") + "</p>")
+</script>
+window.alert("Hello, World!");
 
-        <h1>This a Hello World page</h1>
-        <input type="text" name=""/>
-        <input type="number" name=""/>
-        <script type="text/javascript">
-            document.write("Testing the document")
-        </script>
-    </body>
-</html>
+document.write('Hello, World!')
