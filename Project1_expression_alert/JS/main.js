bf4894aee545e93de43d9541dbaf94d9758adaa9
Atoomp <86169325+Atoomp@users.noreply.github.com>
2022-03-23 02:41:18 -0400

    Add files via upload

---
+++ Project1_expression_alert/JS/main.js
@@ -0,0 +1,9 @@
+// This is my first JavaScript code!
+function My_First_Function() {
+    var str = "This is the button text!";
+
+    document.getElementById("Button_Text").innerHTML = str;
+}
+
+window.alert(Button);
+document.write(Button);
